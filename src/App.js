30dae46d7aaefbd459be153dc6b33c00e50db8da
sabikunnaharsafa11 <sabiunnaharsafa92@gmail.com>
2021-10-01 00:20:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Friends from './components/Friends/Friends';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Friend from './components/Friend/Friend';
+import Header from './components/Header/Header';
+import FriendDetail from './components/FriendDetail/FriendDetail';
+import Culture from './components/Culture/Culture';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+      <Header></Header>
+     <Switch>
+
+       <Route exact path="/"> 
+       <Home></Home>
+       </Route>
+      <Route path="/home">
+        <Home></Home>
+      </Route>
+       <Route path="/friends">
+       <Friends></Friends>
+       </Route>
+        <Route path="/friend/:friendId">
+          <FriendDetail></FriendDetail>
+        </Route>
+       <Route exact path="/about">
+        <About></About>
+       </Route>
+       <Route exact path="/about/culture">
+        <Culture></Culture>
+       </Route>
+       <Route path="*">
+         <NotFound></NotFound>
+       </Route>
+     </Switch>
+   </Router>
     </div>
   );
 }
---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+const Friend = (props) => {
+    const{id, name, phone, website, address} = props.friend;
+    const history = useHistory();
+    const friendStyle={
+        border:'3px solid goldenrod',
+        padding:'10px',
+        borderRadius: '10px'
+    };
+    const handleFriendclick =() => {
+     history.push(`/friend/${id}`)
+    }
+    return (
+        <div style={friendStyle}>
+           <h2> I am: {name} {id}</h2> 
+           <h5>Call me: {phone}</h5>
+           <p>Visit me: {website}</p>
+           <p><small>I live in: {address.city}</small></p>
+           <Link to={`/friend/${id}`}>Visit me</Link>
+           <br />
+           <Link to={`/friend/${id}`}>
+           <button>Visit me</button>
+           </Link>
+           <br />
+           <button onClick={handleFriendclick}>Visit me 2</button>
+           
+        </div>
+    );
+};
+
+export default Friend;
